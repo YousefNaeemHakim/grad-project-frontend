4cad460df4fadbb3fffc81b520eea169f0dbabe8
YousefNaeemHakim <yousefnaeem22@gmail.com>
2023-06-23 19:27:26 +0300

    <IGP-87> Redesign the landing page

--- js/script.js
+++ js/script.js
@@ -1,77 +1,112 @@
-$(document).ready(function () {
-    //Active Class 
-    $('.main-links li').click(function(){
-        $('.main-links li').removeClass('active');
-        $(this).addClass('active');
-    });
-    if ($(window).scrollTop() > 230) {
-        $('nav').addClass('fixed-nav');
-    } else {
-        $('nav').removeClass('fixed-nav');
+/*
+
+Style   : MobApp Script JS
+Version : 1.0
+Author  : Surjith S M
+URI     : https://surjithctly.in/
+
+Copyright © All rights Reserved 
+
+*/
+
+$(function() {
+    "use strict";
+
+    /*-----------------------------------
+     * FIXED  MENU - HEADER
+     *-----------------------------------*/
+    function menuscroll() {
+        var $navmenu = $('.nav-menu');
+        if ($(window).scrollTop() > 50) {
+            $navmenu.addClass('is-scrolling');
+        } else {
+            $navmenu.removeClass("is-scrolling");
+        }
     }
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 230) {
-            $('nav').addClass('fixed-nav');
-        } else {
-            $('nav').removeClass('fixed-nav');
+    menuscroll();
+    $(window).on('scroll', function() {
+        menuscroll();
+    });
+    /*-----------------------------------
+     * NAVBAR CLOSE ON CLICK
+     *-----------------------------------*/
+
+    $('.navbar-nav > li:not(.dropdown) > a').on('click', function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+    /* 
+     * NAVBAR TOGGLE BG
+     *-----------------*/
+    var siteNav = $('#navbar');
+    siteNav.on('show.bs.collapse', function(e) {
+        $(this).parents('.nav-menu').addClass('menu-is-open');
+    })
+    siteNav.on('hide.bs.collapse', function(e) {
+        $(this).parents('.nav-menu').removeClass('menu-is-open');
+    })
+
+    /*-----------------------------------
+     * ONE PAGE SCROLLING
+     *-----------------------------------*/
+    // Select all links with hashes
+    $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').not('[data-toggle="tab"]').on('click', function(event) {
+        // On-page links
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function() {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
         }
-        if ($(window).scrollTop() >= $('header').height() / 2) {
-            $('.goTop').fadeIn(1000);
-        } else {
-            $('.goTop').fadeOut(1000);
-        }
     });
-    $('.goTop').click(function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 1000);
-    });
+    /*-----------------------------------
+     * OWL CAROUSEL
+     *-----------------------------------*/
+    var $testimonialsDiv = $('.testimonials');
+    if ($testimonialsDiv.length && $.fn.owlCarousel) {
+        $testimonialsDiv.owlCarousel({
+            items: 1,
+            nav: true,
+            dots: false,
+            navText: ['<span class="ti-arrow-left"></span>', '<span class="ti-arrow-right"></span>']
+        });
+    }
 
+    var $galleryDiv = $('.img-gallery');
+    if ($galleryDiv.length && $.fn.owlCarousel) {
+        $galleryDiv.owlCarousel({
+            nav: false,
+            center: true,
+            loop: true,
+            autoplay: true,
+            dots: true,
+            navText: ['<span class="ti-arrow-left"></span>', '<span class="ti-arrow-right"></span>'],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 3
+                }
+            }
+        });
+    }
 
-    // $(window).scroll(function () {
-    //     $('.box').each(function () {
-    //         if ($(window).scrollTop() > $(this).offset().top - $('nav').height() - 1) { // Not Equal
-    //             var boxId = $(this).attr('id');
-    //             var lastBoxId = $('.box').last().attr('id');
-    //             $('nav li').removeClass('active');
-    //             console.log();
-    //             if ($(window).scrollTop() >= $('#contact-us').offset().top + 1) {
-    //                 $('nav li a[data-scroll="' + lastBoxId + '"]').parent().addClass('active');
-    //             } else {
-    //                 $('nav li a[data-scroll="' + boxId + '"]').parent().addClass('active');
-    //             }
-    //             var lastScroll = $(document).height()  - $(window).height();
-    //             if($(window).scrollTop() + $(window).height() == $(document).height())  {
-    //                 $('nav li a[data-scroll="' + lastBoxId + '"]').parent().addClass('active');
-    //             } else {
-    //                 console.log('No');
-    //             }
-            
-
-    //         }
-    //     });
-
-    // });
-
-    $('nav li a').click(function () {
-        $('html, body').animate({
-            scrollTop: $('#' + $(this).data('scroll')).offset().top - $('nav').height()
-        }, 1000);
-    });
-    
-    
-    
-    $('.scroll-down span').click(function () {
-        $('html, body').animate({
-            scrollTop: $('.about').offset().top - $('nav').height()
-        }, 2000)
-    });
-    
-});
-
-
-let bars = document.querySelector('.bars'),
-    SideBar = document.querySelector('.header ul');
-    bars.addEventListener('click',()=>{
-    SideBar.classList.toggle("active");
-})
+}); /* End Fn */
